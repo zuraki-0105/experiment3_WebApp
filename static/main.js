32015a4e8600791913d3326f6f846964dcfd55e9
zuraki-0105 <tetsuki0105@icloud.com>
2025-12-16 14:32:35 +0900

    分類

--- static/main.js
+++ static/main.js
@@ -19,12 +19,16 @@ async function loadRestaurants() {
 
     console.log("APIから取得したデータ:", restaurants);
 
+    // 初期化
+    markerList.forEach(m => map.removeLayer(m));
+    markerList = [];
+
     // マーカー作成
     restaurants.forEach(r => {
         const marker = L.marker([r.lat, r.lng])
-            .bindPopup(`<b>${r.name}</b><br>${r.address}<br>${r.segment}`);
+            .bindPopup(`<b>${r.name}</b><br>${r.address}<br>${r.category}`);
 
-        marker.segment = r.segment;  // ← フィルタ用に保存
+        marker.category = r.category;  // ← フィルタ用に保存
 
         marker.addTo(map);
         markerList.push(marker);
@@ -34,16 +38,24 @@ async function loadRestaurants() {
 
 // ====== フィルタ処理 ======
 function applyFilter() {
-    const showStudent = document.getElementById("filter-student").checked;
-    const showFamily  = document.getElementById("filter-family").checked;
+    // const showStudent = document.getElementById("filter-student").checked;
+    // const showFamily  = document.getElementById("filter-family").checked;
+    const showConvenience = document.getElementById("filter-convenience").checked;
+    const showCafe        = document.getElementById("filter-cafe").checked;
+    const showDrugstore   = document.getElementById("filter-drugstore").checked;
+    const showSuper       = document.getElementById("filter-super").checked;
 
     markerList.forEach(marker => {
-        const seg = marker.segment;
+        const cate = marker.category;
 
         // 表示条件
         const shouldShow =
-            (seg === "student" && showStudent) ||
-            (seg === "family"  && showFamily);
+            (cate === "student" && showStudent) ||
+            (cate === "family"  && showFamily) ||
+            (cate === "convenience" && showConvenience) ||
+            (cate === "cafe"        && showCafe) ||
+            (cate === "drugstore"   && showDrugstore) ||
+            (cate === "super"       && showSuper);;
 
         if (shouldShow) {
             marker.addTo(map);
@@ -55,8 +67,12 @@ function applyFilter() {
 
 
 // ====== チェックボックスにイベント追加 ======
-document.getElementById("filter-student").addEventListener("change", applyFilter);
-document.getElementById("filter-family").addEventListener("change", applyFilter);
+document.querySelectorAll("#controls input").forEach(cb => {
+    cb.addEventListener("change", applyFilter);
+});
+
+// document.getElementById("filter-student").addEventListener("change", applyFilter);
+// document.getElementById("filter-family").addEventListener("change", applyFilter);
 
 
 // 初期読み込み
